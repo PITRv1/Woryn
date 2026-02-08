2daa98d602ae0a227416d813a55ce52616c38ba7
scynthi <85178488+scynthi@users.noreply.github.com>
2026-02-08 18:17:45 +0100

    ui

--- GameSource/Assets/UI/Cards/PointCard3D/PointCard3d.cs
+++ GameSource/Assets/UI/Cards/PointCard3D/PointCard3d.cs
@@ -4,6 +4,9 @@ using System;
 public partial class PointCard3d : Node3D
 {
 	[Export] Label3D label;
+	[Export] MeshInstance3D outlineMesh;
+
+	Color[] outlineColors = [Colors.Green, Colors.Blue, Colors.Red];
 
 	private PointCard _pointCard;
 	public PointCard PointCard
@@ -18,12 +21,29 @@ public partial class PointCard3d : Node3D
 			GD.Print($"Card rarity: {_pointCard.CardRarity}");
 
 			if (label == null) return;
-			label.Text = _pointCard.PointValue.ToString();
+			UpdateText();
+
+			if (outlineMesh == null) return;
+			UpdateMeshColor();
 		}	
 	}
 
     public override void _Ready()
-    {
+	{
+		// UpdateText();
+		UpdateMeshColor();
+	}
+
+	private void UpdateMeshColor()
+	{
+		StandardMaterial3D material = (StandardMaterial3D)outlineMesh.GetSurfaceOverrideMaterial(0);
+		// material.AlbedoColor = outlineColors[(int)PointCard.CardRarity-1];
+		material.AlbedoColor = outlineColors[0];
+		outlineMesh.SetSurfaceOverrideMaterial(0, material);
+	}
+
+	private void UpdateText()
+	{
         label.Text = _pointCard.PointValue.ToString();
-    }
+	}
 }
