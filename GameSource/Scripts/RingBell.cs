4b08fe4591bf636dab3a71502c943c2fc6f061df
ABEL0206 <wagnerabel95@gmail.com>
2026-02-12 16:36:33 +0100

    Ring bell animation

--- GameSource/Scripts/RingBell.cs
+++ GameSource/Scripts/RingBell.cs
@@ -6,12 +6,14 @@ public partial class RingBell : Node3D, InteractableObjectInterface
 	[Export] MultiplayerPlayerClass multiplayerPlayerClass;
 	[Export] private Area3D _area3D;
 	[Export] private PlayerVisualController playerVisualController;
+	[Export] private AnimationPlayer ringAnimPlayer;
 
 
 	public void UseObject()
 	{
 		multiplayerPlayerClass.SendFoldRequest();
 		playerVisualController.playBellHit();
+		ringAnimPlayer.Play("ring");
 	}
 	
 }
