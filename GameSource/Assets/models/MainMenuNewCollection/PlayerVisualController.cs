45731652c9dd6a52018de6feb86d276e5759c9b6
PITRv1 <petersandor.toth@gmail.com>
2026-02-12 18:52:55 +0100

    I love abilitities

--- GameSource/Assets/models/MainMenuNewCollection/PlayerVisualController.cs
+++ GameSource/Assets/models/MainMenuNewCollection/PlayerVisualController.cs
@@ -3,8 +3,23 @@ using System;
 
 public partial class PlayerVisualController : Node3D
 {
+    [Export] int playerIndex = 1;
     [Export] AnimationTree animationTree;
 
+    [ExportGroup("Bodyparts")]
+    [Export] private Godot.Collections.Array<Material> materials;
+    [Export] private Godot.Collections.Array<MeshInstance3D> bodyparts;
+
+    public override void _Ready()
+    {
+        Material material = materials[playerIndex];
+
+        foreach (MeshInstance3D item in bodyparts)
+        {
+            item.MaterialOverlay = material;
+        }
+    }
+ 
     public void playBellHit()
     {
         animationTree.Set("parameters/OneShot/request", (int)AnimationNodeOneShot.OneShotRequest.Fire);
